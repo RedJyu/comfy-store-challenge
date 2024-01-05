f693b8de69a8227cc9c446939d746f4ba7586e1d
RedJyu <piotr.stezycki@protonmai.com>
2024-01-05 14:44:28 +0100

    pages

--- comfy-store/src/App.jsx
+++ comfy-store/src/App.jsx
@@ -1,3 +1,17 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages';
+
 const App = () => {
   return <h1 className='text-7xl font-bold underline'>Tailwind project</h1>;
 };
