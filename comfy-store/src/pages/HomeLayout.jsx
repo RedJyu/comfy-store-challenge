0d927a5af956bef4f7852f396c40311ef7dc0556
RedJyu <piotr.stezycki@protonmai.com>
2024-01-08 15:32:03 +0100

    navbar

--- comfy-store/src/pages/HomeLayout.jsx
+++ comfy-store/src/pages/HomeLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
   return (
     <>
       <nav>
         <Header />
-        <span className='text-4xl text-primary'>Comfy</span>
+        <Navbar />
       </nav>
       <section className='align-element py-20'>
         <Outlet />
