15ea006a4078ec43ac62db1fc415739f50815243
RedJyu <piotr.stezycki@protonmai.com>
2024-01-08 11:22:10 +0100

    error handle

--- comfy-store/src/pages/Error.jsx
+++ comfy-store/src/pages/Error.jsx
@@ -1,4 +1,28 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404)
+    return (
+      <main className='grid min-h-[100vh] place-items-center px-8'>
+        <div className='text-center'>
+          <p className='text-9xl font-semibold text-primary'>404</p>
+          <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+            Page not found
+          </h1>
+          <div className='mt-10'>
+            <Link to='/' className='btn btn-secondary'>
+              Go Back
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  return (
+    <main className='grid min-h-[100vh] place-items-center px-8'>
+      <h4 className='text-center text-9xl font-bold'>there was an error</h4>
+    </main>
+  );
 };
 export default Error;
