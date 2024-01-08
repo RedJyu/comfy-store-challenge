a0bbd340850cb481483078f0bb3df239430ab1f2
RedJyu <piotr.stezycki@protonmai.com>
2024-01-08 14:40:32 +0100

    register page

--- comfy-store/src/pages/Register.jsx
+++ comfy-store/src/pages/Register.jsx
@@ -1,4 +1,31 @@
+import { FormInput, SubmitBtn } from '../components';
+import { Form, Link } from 'react-router-dom';
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className='h-screen grid place-items-center'>
+      <Form
+        method='POST'
+        className='card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y4'
+      >
+        <h4 className='text-center text-3xl font-bold'>Register</h4>
+        <FormInput type='text' label='username' name='username' />
+        <FormInput type='email' label='email' name='email' />
+        <FormInput type='password' label='password' name='password' />
+        <div className='mt-4 mb-4'>
+          <SubmitBtn text='login' />
+        </div>
+        <p className='text-center'>
+          Already member?
+          <Link
+            to='/login'
+            className='ml-2 link link-hover link-primary capitalize'
+          >
+            Login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 export default Register;
