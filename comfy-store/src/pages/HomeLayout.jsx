f031fde423cfa7248f0359a7ed6ca3437edd779a
RedJyu <piotr.stezycki@protonmai.com>
2024-01-08 14:57:11 +0100

    header added

--- comfy-store/src/pages/HomeLayout.jsx
+++ comfy-store/src/pages/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 const HomeLayout = () => {
   return (
     <>
       <nav>
+        <Header />
         <span className='text-4xl text-primary'>Comfy</span>
       </nav>
-      <Outlet />
+      <section className='align-element py-20'>
+        <Outlet />
+      </section>
     </>
   );
 };
