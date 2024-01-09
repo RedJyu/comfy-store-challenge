0869b6d30461bee15c2bdfcc21727804e2a8ff5c
RedJyu <piotr.stezycki@protonmai.com>
2024-01-09 12:53:57 +0100

    theme toggle and about finished

--- comfy-store/src/components/Navbar.jsx
+++ comfy-store/src/components/Navbar.jsx
@@ -2,8 +2,29 @@ import { NavLink } from 'react-router-dom';
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import NavLinks from './NavLinks';
+import { useState, useEffect } from 'react';
 
+const themes = {
+  cupcake: 'cupcake',
+  dark: 'dark',
+};
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem('theme') || themes.cupcake;
+};
 const Navbar = () => {
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+
+  const handleTheme = () => {
+    const { cupcake, dark } = themes;
+    const newTheme = theme === cupcake ? dark : cupcake;
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className='bg-base-200'>
       <div className='navbar align-element'>
@@ -33,7 +54,16 @@ const Navbar = () => {
           </ul>
         </div>
         <div className='navbar-end'>
-          {/* theme */}
+          <label className='swap swap-rotate'>
+            {/* this hidden checkbox controls the state */}
+            <input type='checkbox' onChange={handleTheme} />
+
+            {/* sun icon */}
+            <BsSunFill className='swap-on h-4 w-4' />
+
+            {/* moon icon */}
+            <BsMoonFill className='swap-off h-4 w-4' />
+          </label>
 
           <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
             <div className='indicator'>
