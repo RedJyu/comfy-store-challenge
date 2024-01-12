17a67d5dd278887bb963d9dbc9369097ae8ce2f4
RedJyu <piotr.stezycki@protonmai.com>
2024-01-12 11:40:10 +0100

    singleProduct update

--- comfy-store/src/pages/SingleProduct.jsx
+++ comfy-store/src/pages/SingleProduct.jsx
@@ -3,11 +3,47 @@ import { formatPrice, customFetch } from '../utils';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
-export const loader = async () => {
-  return null;
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`);
+  return { product: response.data.data };
 };
 
 const SingleProduct = () => {
-  return <div>SingleProduct</div>;
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const cena = formatPrice(price);
+  return (
+    <section>
+      <div className='text-md breadcrumbs'>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/products'>Products</Link>
+          </li>
+        </ul>
+      </div>
+      {/* product */}
+      <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
+        {/* 1st colum */}
+        <img
+          src={image}
+          alt={title}
+          className='w-96 h-96 object-cover rounded-lg lg:w-full'
+        />
+        {/* 2nd colum/info */}
+        <div>
+          <h1 className='capitalize text-3xl font-bold'>{title}</h1>
+          <h4 className='text-xl text-neutral-content font-bold mt-2'>
+            {company}
+          </h4>
+          <p className='mt-3 text-xl'>{cena}</p>
+          <p className='mt-6 leading-8'>{description}</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default SingleProduct;
