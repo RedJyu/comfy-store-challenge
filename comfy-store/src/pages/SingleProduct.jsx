a2385c7e874c4b1e190df2df7da8d467c0ea6eb0
RedJyu <piotr.stezycki@protonmai.com>
2024-01-12 11:15:46 +0100

    singleProduct

--- comfy-store/src/pages/SingleProduct.jsx
+++ comfy-store/src/pages/SingleProduct.jsx
@@ -1,3 +1,12 @@
+import { useLoaderData } from 'react-router-dom';
+import { formatPrice, customFetch } from '../utils';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+export const loader = async () => {
+  return null;
+};
+
 const SingleProduct = () => {
   return <div>SingleProduct</div>;
 };
