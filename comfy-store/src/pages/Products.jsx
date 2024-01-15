2f6f0a0f914bed1404a97039deebe35f6370d038
RedJyu <piotr.stezycki@protonmai.com>
2024-01-15 13:36:54 +0100

    products and loader

--- comfy-store/src/pages/Products.jsx
+++ comfy-store/src/pages/Products.jsx
@@ -1,4 +1,20 @@
+import { customFetch } from '../utils';
+import { Filters, PaginationContainer, ProductsContainer } from '../components';
+
+export const loader = async () => {
+  const response = await customFetch(`/products`);
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, meta };
+};
+
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  );
 };
 export default Products;
