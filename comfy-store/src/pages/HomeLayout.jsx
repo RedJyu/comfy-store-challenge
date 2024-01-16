c534c1aee4f84e319d0a2269b3925841dfacbdc7
RedJyu <piotr.stezycki@protonmai.com>
2024-01-16 14:01:49 +0100

    created loading spinner

--- comfy-store/src/pages/HomeLayout.jsx
+++ comfy-store/src/pages/HomeLayout.jsx
@@ -1,17 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import { Loading } from '../components';
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
   return (
     <>
-      <nav>
-        <Header />
-        <Navbar />
-      </nav>
-      <section className='align-element py-20'>
-        <Outlet />
-      </section>
+      <Header />
+      <Navbar />
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className='align-element py-20'>
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
