4ca4918e5c024f5a54958dd1aa044fc566347c3e
RedJyu <piotr.stezycki@protonmai.com>
2024-01-16 11:02:47 +0100

    search bar added

--- comfy-store/src/components/Filters.jsx
+++ comfy-store/src/components/Filters.jsx
@@ -1,4 +1,24 @@
+import FormInput from './FormInput';
+import { Form, useLoaderData, Link } from 'react-router-dom';
+
 const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      {/* search */}
+      <FormInput
+        type='search'
+        label='search'
+        name='search'
+        size='input-sm'
+      ></FormInput>
+      {/* buttons */}
+      <button type='submit' className='btn btn-primary btn-sm'>
+        search
+      </button>
+      <Link to='/products' className='btn btn-accent btn-sm'>
+        reset
+      </Link>
+    </Form>
+  );
 };
 export default Filters;
