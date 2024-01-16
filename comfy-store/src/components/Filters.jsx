43487e30a123be819cf6074929fc1163e17f4313
RedJyu <piotr.stezycki@protonmai.com>
2024-01-16 14:26:53 +0100

    filters finished

--- comfy-store/src/components/Filters.jsx
+++ comfy-store/src/components/Filters.jsx
@@ -5,7 +5,8 @@ import FormSelect from './FormSelect';
 import { Form, useLoaderData, Link } from 'react-router-dom';
 
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
       {/* search */}
@@ -14,6 +15,7 @@ const Filters = () => {
         label='search'
         name='search'
         size='input-sm'
+        defaultValue={search}
       ></FormInput>
       {/* categories */}
 
@@ -22,6 +24,7 @@ const Filters = () => {
         name='category'
         list={meta.categories}
         size='select-sm'
+        defaultValue={category}
       />
       {/* companies */}
       <FormSelect
@@ -29,6 +32,7 @@ const Filters = () => {
         name='company'
         list={meta.companies}
         size='select-sm'
+        defaultValue={company}
       />
       {/* order */}
       <FormSelect
@@ -36,11 +40,22 @@ const Filters = () => {
         name='order'
         list={['a-z', 'z-a', 'high', 'low']}
         size='select-sm'
+        defaultValue={order}
       />
       {/* price range */}
-      <FormRange name='price' label='select price' size='range-sm' />
+      <FormRange
+        name='price'
+        label='select price'
+        size='range-sm'
+        price={price}
+      />
       {/* shipping */}
-      <FormCheckbox name='shipping' label='free shipping' size='checkbox-sm' />
+      <FormCheckbox
+        name='shipping'
+        label='free shipping'
+        size='checkbox-sm'
+        defaultValue={shipping}
+      />
       {/* buttons */}
       <button type='submit' className='btn btn-primary btn-sm'>
         search
