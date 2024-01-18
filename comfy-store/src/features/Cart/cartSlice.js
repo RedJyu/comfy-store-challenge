6c2fa9ec29c5b4bb300d300078b259347cfc2f2d
RedJyu <piotr.stezycki@protonmai.com>
2024-01-18 12:04:27 +0100

    cartSlice finished

--- comfy-store/src/features/Cart/cartSlice.js
+++ comfy-store/src/features/Cart/cartSlice.js
@@ -9,3 +9,19 @@ const defaultState = {
   tax: 0,
   orderTotal: 0,
 };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: defaultState,
+  reducers: {
+    addItem: (state, action) => {
+      console.log(action.payload);
+    },
+    clearCart: (state) => {},
+    removeItem: (state, action) => {},
+    editItem: (state, action) => {},
+  },
+});
+
+export const { addItem, clearCart, removeItem, editItem } = cartSlice.actions;
+export default cartSlice.reducer;
