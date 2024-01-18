704fa2bdd5bf02fe0715f808176db4d6f82bccf5
RedJyu <piotr.stezycki@protonmai.com>
2024-01-18 12:19:57 +0100

    created store.js

---
+++ comfy-store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './src/features/Cart/cartSlice';
+export const store = configureStore({
+  reducer: {
+    cartState: cartReducer,
+  },
+});
