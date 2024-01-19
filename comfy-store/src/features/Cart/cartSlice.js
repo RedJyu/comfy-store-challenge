e2cd637d5ddb82935f8401e552e5a0aecca1e9d2
RedJyu <piotr.stezycki@protonmai.com>
2024-01-19 13:44:03 +0100

    cart casereducer

--- comfy-store/src/features/Cart/cartSlice.js
+++ comfy-store/src/features/Cart/cartSlice.js
@@ -10,9 +10,13 @@ const defaultState = {
   orderTotal: 0,
 };
 
+const getCartFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('cart')) || defaultState;
+};
+
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: defaultState,
+  initialState: getCartFromLocalStorage(),
   reducers: {
     addItem: (state, action) => {
       const { product } = action.payload;
@@ -24,8 +28,9 @@ const cartSlice = createSlice({
       }
       state.numItemsInCart += product.amount;
       state.cartTotal += product.price * product.amount;
-      state.tax = 0.23 * state.cartTotal;
-      state.orderTotal = state.cartTotal + state.shipping + state.tax;
+
+      cartSlice.caseReducers.calculateTotals(state);
+
       localStorage.setItem('cart', JSON.stringify(state));
       toast.success('Item added to cart');
     },
@@ -33,6 +38,10 @@ const cartSlice = createSlice({
     clearCart: (state) => {},
     removeItem: (state, action) => {},
     editItem: (state, action) => {},
+    calculateTotals: (state) => {
+      state.tax = 0.23 * state.cartTotal;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
+    },
   },
 });
 
