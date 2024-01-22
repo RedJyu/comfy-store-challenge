31b167f87a4586ffb370a554b613e49f8c7a1d17
RedJyu <piotr.stezycki@protonmai.com>
2024-01-22 14:28:25 +0100

    cart totals finished

--- comfy-store/src/components/CartTotals.jsx
+++ comfy-store/src/components/CartTotals.jsx
@@ -2,6 +2,34 @@ import { useSelector } from 'react-redux';
 import { formatPrice } from '../utils';
 
 const CartTotals = () => {
-  return <h1 className='text-3xl'> CartTotals</h1>;
+  const { cartTotal, shipping, tax, orderTotal } = useSelector(
+    (state) => state.cartState
+  );
+  return (
+    <div className='card bg-base-300'>
+      <div className='card-body'>
+        {/* subtotal */}
+        <p className='flex justify-between text-xs border-b border-base300 pb-2'>
+          <span>Subtotal</span>
+          <span className='font-medium'>{formatPrice(cartTotal)}</span>
+        </p>
+        {/* shipping */}
+        <p className='flex justify-between text-xs border-b border-base300 pb-2'>
+          <span>Shipping</span>
+          <span className='font-medium'>{formatPrice(shipping)}</span>
+        </p>
+        {/* tax */}
+        <p className='flex justify-between text-xs border-b border-base300 pb-2'>
+          <span>Tax</span>
+          <span className='font-medium'>{formatPrice(tax)}</span>
+        </p>
+        {/* total */}
+        <p className='mt-4 flex justify-between text-sm pb-2'>
+          <span className='font-bold'>Order Total</span>
+          <span>{formatPrice(orderTotal)}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 export default CartTotals;
