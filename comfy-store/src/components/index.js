43e253a4db2b712108dc01bb7f44634c8af89540
RedJyu <piotr.stezycki@protonmai.com>
2024-01-22 12:44:33 +0100

    shopping cart

--- comfy-store/src/components/index.js
+++ comfy-store/src/components/index.js
@@ -14,3 +14,5 @@ export { default as ProductsContainer } from './ProductsContainer';
 export { default as ProductList } from './ProductList';
 export { default as FormSelect } from './FormSelect';
 export { default as Loading } from './Loading';
+export { default as CartItemsList } from './CartItemsList';
+export { default as CartTotals } from './CartTotals';
--- comfy-store/src/pages/Cart.jsx
+++ comfy-store/src/pages/Cart.jsx
@@ -1,4 +1,13 @@
+import { useSelector } from 'react-redux';
+import { CartItemsList, SectionTitle, CartTotals } from '../components';
+import { Link } from 'react-router-dom';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const user = null;
+  const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
+  if (numItemsInCart === 0) {
+    return <SectionTitle text='your cart is empty' />;
+  }
+  return <SectionTitle text=' Your Cart' />;
 };
 export default Cart;
