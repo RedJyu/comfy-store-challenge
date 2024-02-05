297db2e0969c217a6702ed2a09caa0f1e100a63d
RedJyu <piotr.stezycki@protonmai.com>
2024-02-05 13:01:24 +0100

    orders

--- comfy-store/src/pages/Orders.jsx
+++ comfy-store/src/pages/Orders.jsx
@@ -6,7 +6,32 @@ import { OrdersList, PaginationContainer, SectionTitle } from '../components';
 export const loader =
   (store) =>
   async ({ request }) => {
-    return null;
+    const user = store.getState().userState.user;
+    if (!user) {
+      toast.warn('You must be logged in');
+      return redirect('/login');
+    }
+    const params = Object.fromEntries([
+      // for pagination
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    try {
+      const response = await customFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      console.log(response);
+      return { orders: response.data.data, meta: response.data.meta };
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'there was an error with your order';
+      toast.error(errorMessage);
+      if (error.response.status === 401 || 403) return redirect('/login');
+      return null;
+    }
   };
 
 const Orders = () => {
